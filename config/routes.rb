86c13f5dda4203382d664f800bb6241695598bf1
Germán Trigo Cortés <contacto@alphacorp.cl>
2022-06-16 15:26:38 -0400

    Segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#index"
   get  "users/login"
   get  "pages/about_us"
-  root "pages#index"
+  get  "pages/test"
 end
